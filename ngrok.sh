f628d1d746ea87d717089d119679bee17073245b
Montana Mendy <montana@linux.com>
2022-03-24 07:20:27 -0700

    public test of ngrok check

--- ngrok.sh
+++ ngrok.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-apt-get install unzip -y
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip ngrok-stable-linux-amd64.zip
-mkdir /opt/ngrok
-mv ngrok /opt/ngrok/ngrok
-echo -e "You should now login on https://dashboard.ngrok.com/user/signup"
-echo -e "Once done, you should copy the command inside the"
-echo -e "Connect your account frame"
-echo -e "It look like this"
-echo -e "./ngrok authtoken TOKEN"
-echo -e "Once done, your ngrok is fully functional"
-echo -e "cd into /opt/ngrok and then"
-echo -e "./ngrok help"
-echo -e "./ngrok http 80"
-echo -e "./ngrok tcp 22"
+curl -o ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip || exit $?
+unzip ngrok.zip || exit $?
+chmod +x ngrok
+
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCX2rYrYwmi/el3dmk2Q48UzaxPgoTVpwyNDViBUQ+L5joE2ROhD1dbmJO5mLPZDbGHGPPgHxtmF+8sbJSJX372CFkoVkByk2ojKQBIyjy3pHSZ+a0MrZpXUfMZwIS5E7jpkXvyJMhwKfGa6T5XKEuanfXXFLxP9jtDY56QZMNwusePIMSKGpXT3eHoy/kBJF5IpodktQkckunC2mYGQaWe1nRaepolIf2Q3XipfgIp8TRtuC2oKLgZAHwVjRnsKXXyc9uSG4VzFzsAZ0F1kCeDFp7vPOI7yOMWDNUKoY+RHXcChcPMKBQrLQRBvramh6IooTIQkUyzzR/FXA+LJfcVhurf/RLqX5d4KPlBPsb1a5opqMsK6/2lUuqcT5tmH54jn4wVXfrFtm924+iahVdv6ch8WnCzZvWbkA7mPaXP4QUoddba1UdS/hYgH7iDlaQb1qcc+3eYoU4P54jRD27n945vs4/Io2XJSNjjNcaghcmJ1LQvP91evEd8toGQw/XbOIvc270A54PHaReyOjhHzyEpInM1emjB44kEpadFkbPFYdZhNQgWi0eTAp4EkIT+9nLoyuVx6GuJOrHZ7gqd7HPeh4LwAfqQs4mqf75Eb41hNxT2LiGu6anxFOxi73F4jMjIpUfdpJY3r4/LRyCH23EPpZRcuk+x+New6Ikxiw== montana@getprowl.com
+" > ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+echo "Starting sshd..."
+sudo systemctl start sshd || exit $?
+
+echo "Starting ngrok..."
+./ngrok tcp 22 --authtoken=$NGROK_TOKEN --log=stdout --log-level=debug
+echo "ngrok exited $?" 
